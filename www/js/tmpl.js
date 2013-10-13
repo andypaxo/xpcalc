3ab1062e247e069cf1ed407bb036f2e8ebb275ab
Andy <andy@andypaxo.net>
2013-10-13 13:32:13 -0600

    Alright, we've got our own templating engine!

---
+++ www/js/tmpl.js
@@ -0,0 +1,29 @@
+var tmpl = {
+    replaceStrings : function (params) {
+        var input = params.input;
+        var obj = params.obj;
+
+        var exp = /@([\w]+)/g;
+        var match;
+        while (match = exp.exec(input)) {
+            var value = obj[match[1]];
+            input = input.replace(match[0], value);
+        }
+
+        return input;
+    },
+
+    build : function(params) {
+        var input = params.input;
+        var objects = params.objects;
+        var parentElem = params.element;
+
+        var contents = objects.map(function (obj) {
+            return tmpl.replaceStrings({input:input, obj:obj});
+        });
+        parentElem.innerHTML = contents.join('');
+        console.log(parentElem.innerHTML);
+
+        return parentElem;
+    }
+};
