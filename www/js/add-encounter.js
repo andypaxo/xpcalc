c50ac910384ad62a9c63216dc56127435c039bb6
Andy <andy@andypaxo.net>
2013-10-15 19:42:11 -0600

    Encounter sheet is actually calculating XPs!

--- www/js/add-encounter.js
+++ www/js/add-encounter.js
@@ -20,8 +20,21 @@
         encounter.foes.forEach(function (foe, index) {
             var newListItem = document.createElement('li');
             newListItem.innerHTML = document.getElementById('tmpl-foe').innerHTML;
-            util.findInput(newListItem, 'foe-challenge-rating').value = foe.challengeRating;
-            util.findInput(newListItem, 'foe-quantity').value = foe.quantity;
+            
+            var crInput = util.findInput(newListItem, 'foe-challenge-rating');
+            crInput.value = foe.challengeRating;
+            crInput.onchange = function () {
+                foe.challengeRating = crInput.value;
+                calculateScores();
+            };
+            
+            var qInput = util.findInput(newListItem, 'foe-quantity')
+            qInput.value = foe.quantity;
+            qInput.onchange = function () {
+                foe.quantity = qInput.value;
+                calculateScores();
+            };
+
             document.getElementById('list-foes').appendChild(newListItem);
         });
     };
@@ -32,6 +45,25 @@
             objects: encounter.party,
             element: document.getElementById('list-characters')
         });
+        Array.prototype.forEach.call(
+            document.getElementsByClassName('character-enable'),
+            function (elem) {
+                elem.onclick = function () {
+                    var charId = elem.getAttribute('name');
+                    var character = encounter.party.filter(function (c) { return c.id === charId; })[0];
+                    character.include = elem.checked;
+                    calculateScores();
+                };
+            });
+    };
+
+    var calculateScores = function () {
+        var result = calculator.partyXP(encounter);
+        for(var playerId in result) {
+            var player = encounter.party.filter(function (player) { return player.id === playerId; })[0];
+            player.xpGain = result[playerId];
+        };
+        buildCharacterListing();
     };
 
     window.onload = function () {
@@ -39,7 +71,7 @@
         encounter.party = repo.fetch(campaignId.replace('campaign', 'characters'));
 
         buildFoeListing();
-        buildCharacterListing();
+        calculateScores();
 
         document.getElementById('btn-done').onclick = function () {
             window.history.back();
@@ -49,6 +81,7 @@
         document.getElementById('btn-add-foe').onclick = function () {
             encounter.foes.push({challengeRating:1, quantity:1});
             buildFoeListing();
+            calculateScores();
             return false;
         };
     };
