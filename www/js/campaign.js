85d765da7da8452e41b64ab07204615d937abb14
Andy <andy@andypaxo.net>
2013-11-03 13:01:32 -0700

    Use iDangero.us Swiper to get horizontal paging for characters / encounters

--- www/js/campaign.js
+++ www/js/campaign.js
@@ -1,8 +1,48 @@
 (function() {
+    var countFoes = function(encounter) {
+        var count = encounter.foes.reduce(function (curr, foe) {
+            return curr + Number(foe.quantity);
+        }, 0);
+        return count + ' foe' + (count == 1 ? '' : 's');
+    };
+
+    var attachClickHandler = function(elem, campaignId, encounterId) {
+        elem.onclick = function () {
+            document.location = 'add-encounter.html?campaignId=' + campaignId + '&id=' + encounterId;
+        };
+    }
+
     window.onload = function () {
         var campaignId = util.getQueryStringParam('id');
         var campaign = repo.fetch(campaignId);
         document.getElementById('title-campaign-name').innerText = campaign.name;
+
+        // -------------
+
+        var listingElement = document.getElementById('list-encounters');
+
+        var encounters = repo.fetch(campaignId.replace('campaign', 'encounters')) || [];
+        encounterListing = encounters.map(function (encounter) {
+            return {
+                id : encounter.id,
+                when : humanized_time_span(new Date(encounter.date)),
+                foeCount : countFoes(encounter)
+            };
+        });
+
+        tmpl.build({
+            element: listingElement,
+            objects: encounterListing,
+            input: '<li class="encounter-item" data-id="@id"><span class="item-description">@when</span><br/><span class="item-subtext">@foeCount</span></li>'
+        });
+
+        var elems = document.getElementsByClassName('encounter-item');
+        for (var i = elems.length - 1; i >= 0; i--) {
+            var encounterId = elems[i].getAttribute('data-id');
+            attachClickHandler(elems[i], campaignId, encounterId);
+        };
+
+        // ------------------
 
         var characters = repo.fetch(campaignId.replace('campaign', 'characters')) || [];
         characters.forEach(function (char) {
@@ -36,8 +76,11 @@
             document.location = 'start-campaign.html?campaignId=' + campaignId;
         };
 
-        document.getElementById('btn-edit-encounters').onclick = function () {
-            document.location = 'list-encounters.html?campaignId=' + campaignId;
-        };
+        // ------------
+
+        //Swipe(document.getElementById('content'));
+        new Swiper('.swiper-container', {
+            mode: 'horizontal'
+        });
     };
 })();
